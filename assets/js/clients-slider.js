466239c1f2e888ab43eabf821e136fa2350ae16a
FilipeeOliveira <filipe.jx.10@gmail.com>
2024-04-17 12:58:06 -0400

    include slider clients and testimonial

---
+++ assets/js/clients-slider.js
@@ -0,0 +1,39 @@
+(function() {
+  "use strict";
+
+  // Clients Slider
+  new Swiper('.clients-slider', {
+    speed: 400,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '#swiper-pagination-clients',
+      type: 'bullets',
+      clickable: true
+    },
+    breakpoints: {
+      320: { slidesPerView: 2, spaceBetween: 40 },
+      480: { slidesPerView: 3, spaceBetween: 60 },
+      640: { slidesPerView: 4, spaceBetween: 80 },
+      992: { slidesPerView: 6, spaceBetween: 120 }
+    }
+  });
+
+  // Animation on scroll
+  window.addEventListener('load', () => {
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    });
+  });
+
+  // Initiate Pure Counter 
+  new PureCounter();
+
+})();
