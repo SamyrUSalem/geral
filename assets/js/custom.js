60c259600156a6bb06bc42730f42f35e1914fd5e
SamyrUSalem <samyruchoasalem@hotmail.com>
2024-04-25 12:00:39 -0400

    Imagens alteradas, foi criado um modal para a vizualização do video e esta sendo feito as configurações do form

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -195,8 +195,28 @@
       }
     });
   }
-
-
-
-
 })(window.jQuery);
+
+(function () {
+  "use strict";
+
+  /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
+
+  /**
+   * Initiate glightbox 
+   */
+  const glightbox = GLightbox({
+    selector: '.glightbox'
+  });
+
+})()
